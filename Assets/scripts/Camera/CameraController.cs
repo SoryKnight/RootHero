76d46779567936cfe49969f44e54d658c86d6ade
jsoriano <soryknight@gmail.com>
2023-02-04 18:59:33 +0100

    Solving a bug in player

--- Assets/scripts/Camera/CameraController.cs
+++ Assets/scripts/Camera/CameraController.cs
@@ -5,21 +5,15 @@ using System.Collections;
 public class CameraController : MonoBehaviour
     {
     public Transform camPosition;
-    public Transform futurePosition;
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
-            Debug.Log("Set " + name + " as future position");
-            futurePosition = camPosition;            
-            Camera.main.transform.position = futurePosition.position;
-            Camera.main.transform.rotation = futurePosition.rotation;
+            Debug.Log("Set " + name + " as future position");          
+            Camera.main.transform.position = camPosition.position;
+            Camera.main.transform.rotation = camPosition.rotation;
             }
         }
     void OnTriggerExit(Collider other) {
             Debug.Log("Exiting " + name + " so moving to new position");
-            if (futurePosition != camPosition) {
-                Camera.main.transform.position = futurePosition.position;
-                Camera.main.transform.rotation = futurePosition.rotation;
-            }
     }
 
 }
