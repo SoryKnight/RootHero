2cd2c832cfde61e3fae73c764ce18d59067e0273
Adri <theroso@gmail.com>
2023-02-04 20:12:36 +0100

    I hate unity and git

--- Assets/scripts/Camera/CameraController.cs
+++ Assets/scripts/Camera/CameraController.cs
@@ -5,15 +5,21 @@ using System.Collections;
 public class CameraController : MonoBehaviour
     {
     public Transform camPosition;
+    public Transform futurePosition;
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
-            Debug.Log("Set " + name + " as future position");          
-            Camera.main.transform.position = camPosition.position;
-            Camera.main.transform.rotation = camPosition.rotation;
+            Debug.Log("Set " + name + " as future position");
+            futurePosition = camPosition;            
+            Camera.main.transform.position = futurePosition.position;
+            Camera.main.transform.rotation = futurePosition.rotation;
             }
         }
     void OnTriggerExit(Collider other) {
             Debug.Log("Exiting " + name + " so moving to new position");
+            if (futurePosition != camPosition) {
+                Camera.main.transform.position = futurePosition.position;
+                Camera.main.transform.rotation = futurePosition.rotation;
+            }
     }
 
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller;
-    private Vector3 direction;
-    public float speed = 8;
-    public float jumpForce = 6;
-    public float gravity = -10;
-    public bool ableToMakeADoubleJump = true;
-
-    public Animator animator;
-    public Transform model;
-
-    public static string trait = "None";
     // Start is called before the first frame update
     void Start()
     {
@@ -24,77 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Take the horizontal input to move the player
-        float hInput = Input.GetAxis("Horizontal") * (trait == "Dyslexia" ? -1 : 1);
-        direction.x = hInput * speed;
-        //animator.SetFloat("speed", Mathf.Abs(hInput));
-
-        //Check if the player is on the ground
-        bool grounded = controller.isGrounded;
-        //animator.SetBool("isGrounded", grounded);
-        if (grounded)
-        {
-            direction.y = -1;
-            ableToMakeADoubleJump = true;
-            if (Input.GetButton("Jump"))
-            {
-                Jump();
-            }
-
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                //animator.SetTrigger("fireBallAttack");
-            }
-
-            if (Input.GetKeyDown(KeyCode.G))
-            {
-                Debug.Log("DEBUG KEY!");
-                Debug.Log(model.forward);
-                Vector3 point = transform.position + model.forward + new Vector3(0.0f, -1.25f, 0.0f);
-                Debug.Log(point);
-                Collider[] intersecting = Physics.OverlapSphere(point, 0.05f);
-                Debug.Log(intersecting.Length);
-            }
-        }
-        else
-        {
-            direction.y += gravity * Time.deltaTime;//Add Gravity
-            if (ableToMakeADoubleJump && Input.GetButtonDown("Jump"))
-            {
-                DoubleJump();
-            }
-        }
-
-        /*if (animator.GetCurrentAnimatorStateInfo(0).IsName("Fireball Attack"))
-            return;*/
-
-        //Flip the player
-        if(hInput != 0)
-        {
-            Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
-            model.rotation = newRotation;
-        }
-
-        //Move the player using the character controller
-        controller.Move(direction * Time.deltaTime);
-
-        //Reset Z Position
-        if (transform.position.z != -1f)
-            transform.position = new Vector3(transform.position.x, transform.position.y, -1f);
-
-    }
-
-    private void DoubleJump()
-    {
-        //Double Jump
-        //animator.SetTrigger("doubleJump");
-        direction.y = jumpForce;
-        ableToMakeADoubleJump = false;
-    }
-    private void Jump()
-    {
-        //Jump
-        direction.y = jumpForce;
+        
     }
 
 }
