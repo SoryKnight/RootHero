697526afd0779921afb6c65fa90bb7d40311d7b8
jsoriano <soryknight@gmail.com>
2023-02-04 18:40:02 +0100

    Solve the bugs in SceneChanger

--- Assets/scripts/Camera/SceneChanger.cs
+++ Assets/scripts/Camera/SceneChanger.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
-    void private void OnTriggerEnter(Collider other) {
-        if (other.tag('Player'){
+    void  OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
             SceneManager.LoadScene("TombRoom");
-        })
+        }
     }
+}
