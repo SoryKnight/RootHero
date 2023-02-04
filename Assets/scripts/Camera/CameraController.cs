fb1901daa1defc8e0c5da77ca354002d370a1457
jsoriano <soryknight@gmail.com>
2023-02-04 17:18:55 +0100

    Add triggers and camera angles to Level 1

---
+++ Assets/scripts/Camera/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+    {
+    public Transform camPosition;
+    public Transform futurePosition;
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            Debug.Log("Set " + name + " as future position");
+            futurePosition = camPosition;            
+            Camera.main.transform.position = futurePosition.position;
+            Camera.main.transform.rotation = futurePosition.rotation;
+            }
+        }
+    void OnTriggerExit(Collider other) {
+            Debug.Log("Exiting " + name + " so moving to new position");
+            if (futurePosition != camPosition) {
+                Camera.main.transform.position = futurePosition.position;
+                Camera.main.transform.rotation = futurePosition.rotation;
+            }
+    }
+
+}
